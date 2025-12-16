ab6b4ba9895eeb02f7aa7bbad8c8bf3819778c57
barbabryh <varia.lamraro@gmail.com>
2025-12-16 15:22:19 +0300

    4

--- vite-project/script.js
+++ vite-project/script.js
@@ -1,4 +1,4 @@
-// Интерактив для кнопок "Добавить"
+// Кнопки "Добавить/Удалить"
 const addButtons = document.querySelectorAll('.add-btn');
 const totalElem = document.getElementById('total');
 let totalSum = 0;
@@ -6,20 +6,34 @@ let totalSum = 0;
 addButtons.forEach(button => {
     button.addEventListener('click', (e) => {
         const row = e.target.closest('tr');
-        const price = parseInt(row.children[3].textContent);
-        totalSum += price;
+        const price = parseInt(row.children[3].textContent.replace(/\s/g, '')); 
+
+        if (row.classList.contains('added')) {
+            totalSum -= price;
+            row.classList.remove('added');
+            button.textContent = 'Добавить';
+        } else {
+            totalSum += price;
+            row.classList.add('added');
+            button.textContent = 'Удалить';
+        }
+
         totalElem.textContent = `${totalSum} ₽`;
-        row.classList.add('added');
-        button.disabled = true;
-        button.textContent = 'Добавлено';
     });
 });
 
-// Интерактив для сайдбара
 const toggleBtn = document.getElementById('toggleSidebar');
 const sidebar = document.querySelector('.sidebar');
 
 toggleBtn.addEventListener('click', () => {
+
     sidebar.classList.toggle('hidden');
-    toggleBtn.textContent = sidebar.classList.contains('hidden') ? 'Показать акции' : 'Скрыть акции';
+    
+
+    if (sidebar.classList.contains('hidden')) {
+        toggleBtn.textContent = 'Показать акции';
+    } else {
+        toggleBtn.textContent = 'Скрыть акции';
+    }
 });
+
